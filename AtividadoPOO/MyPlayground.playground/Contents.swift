c9398e6d6eac5cb14623562031e4a16315916a21
Lucas Mafra <lucasmafrapinheiro@gmail.com>
2023-10-20 01:07:13 -0300

    POO Bank implement heritage

--- AtividadoPOO/MyPlayground.playground/Contents.swift
+++ AtividadoPOO/MyPlayground.playground/Contents.swift
@@ -38,15 +38,25 @@ class BankAccount {
     }
 }
 
+class InterestAccount: BankAccount {
+    var interestRate: Double
+    
+    init(number: Int, openingBalance: Double, interestRate: Double) {
+            self.interestRate = interestRate
+            super.init(number: number, openingBalance: openingBalance, type: .saving)
+    }
+}
 
-struct Bank {
+
+class Bank {
     var accounts: [BankAccount] = []
     
-    mutating func addAccount(_ account: BankAccount) {
+    func addAccount(_ account: BankAccount) {
         accounts.append(account)
     }
     
-    func transferAsync(from account1: inout BankAccount, to account2: inout BankAccount, value: Double) async -> Bool {
+    
+    func transferAsync(from account1: BankAccount, to account2: BankAccount, value: Double) async -> Bool {
         if account1.balance >= value {
             account1.withdraw(value: value)
             account2.deposit(value: value)
@@ -61,19 +71,19 @@ struct Bank {
 var acc1 = BankAccount(number: 1, openingBalance: 900.0, type: .current)
 acc1.deposit(value: 500.0)
 acc1.withdraw(value: 200.0)
-
+acc1.withdraw(value: 500.0)
 let transactions = acc1.filterTransactions { $0.contains("Saque") }
 print("Transações de saque: \(transactions)")
 
 
 
-var acc2 = BankAccount(number: 2, openingBalance: 2000.0, type: .saving)
+var acc2 = InterestAccount(number: 2, openingBalance: 2000.0, interestRate: 0.02)
 var bank = Bank()
 bank.addAccount(acc1)
 bank.addAccount(acc2)
 
 Task {
-    let success = await bank.transferAsync(from: &acc1, to: &acc2, value: 300)
+    let success = await bank.transferAsync(from: acc1, to: acc2, value: 300)
     if success {
        print("Transferência concluída com sucesso")
     } else {
@@ -82,3 +92,5 @@ Task {
 }
 
 acc1.balance
+
+
